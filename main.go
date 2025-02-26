d926a473d56d044d23abc34e9f9a0420270f8ed4
nakmaykin <nakmaykin@sfxdx.com>
2025-02-26 07:08:35 +0700

    refactor: removes all unnecessary things from the main function

--- main.go
+++ main.go
@@ -1,36 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"github.com/Arenelin/List-of-current-affairs/src/middlewares"
 	"github.com/Arenelin/List-of-current-affairs/src/models"
+	"github.com/Arenelin/List-of-current-affairs/src/routes"
 	"github.com/Arenelin/List-of-current-affairs/src/utils"
-	t "github.com/Arenelin/List-of-current-affairs/types"
 	"log"
-
-	"github.com/gofiber/fiber/v3"
 )
 
 func main() {
 	utils.LoadEnv()
-	app := fiber.New()
-
-	app.Post("/login", func(ctx fiber.Ctx) error {
-		loginData := new(t.AuthDTO)
-
-		if err := ctx.Bind().JSON(&loginData); err != nil {
-			return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"error": "User should have a email and password",
-			})
-		}
-		fmt.Println(loginData)
-		return ctx.Status(fiber.StatusOK).JSON(loginData)
-	})
-
-	app.Get("/profile", func(c fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
-
 	models.OpenDatabaseConnection()
+	models.AutoMigrateModels()
+	router := routes.SetupRoutes()
+	middlewares.RegisterMiddlewares(router)
 
-	log.Fatal(app.Listen(":3000"))
+	log.Fatal(router.Listen(":3000"))
 }
